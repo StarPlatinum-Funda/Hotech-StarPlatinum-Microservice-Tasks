943012201a06880f4b0152b8dbd90d0669d557f5
Pedro Guia <u202212010@upc.edu.pe>
2025-06-19 17:10:53 -0500

    chore: initial commit.

---
+++ src/main/java/com/github/hotechbackend/HotechStarPlatinumMicroserviceTasksApplication.java
@@ -0,0 +1,13 @@
+package com.github.hotechbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotechStarPlatinumMicroserviceTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotechStarPlatinumMicroserviceTasksApplication.class, args);
+    }
+
+}
