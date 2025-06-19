1f676c4b8abaf93335a79ee2a675d8c1cbabd9d9
Pedro Guia <u202212010@upc.edu.pe>
2025-06-19 17:28:34 -0500

    feat(iam): add bcrypt hashing service.

---
+++ src/main/java/com/github/hotechbackend/iam/infrastructure/hashing/bcrypt/BCryptHashingService.java
@@ -0,0 +1,8 @@
+package com.github.hotechbackend.iam.infrastructure.hashing.bcrypt;
+
+import com.github.hotechbackend.iam.application.internal.outboundservices.hashing.HashingService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public interface BCryptHashingService  extends HashingService, PasswordEncoder {
+
+}
