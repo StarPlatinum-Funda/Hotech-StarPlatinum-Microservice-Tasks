b42548710d2a9845cbe4429077f7df033adcf0a1
Pedro Guia <u202212010@upc.edu.pe>
2025-06-19 17:33:26 -0500

    feat(iam): add authenticated user resource.

---
+++ src/main/java/com/github/hotechbackend/iam/interfaces/rest/resources/AuthenticatedUserResource.java
@@ -0,0 +1,6 @@
+package com.github.hotechbackend.iam.interfaces.rest.resources;
+
+import java.util.List;
+
+public record AuthenticatedUserResource(Long id, String username, List<String> roles, String token) {
+}
